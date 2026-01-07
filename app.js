f3dadd2a82eddce4ecb27911b7659858ea5ad9dc
Raj Shekhar Singh <rshekhar21@aanyasolutions.net>
2026-01-07 05:45:29 -0500

    1B commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 6100;
+const port = process.env.PORT || 5000;
 const path = require('path');
 const os = require('os');
 const ejs = require('ejs');
